10f373f9061f60068c000397b61d74e124cb7788
LaurStanciu <laur.dutza@yahoo.com>
2017-04-28 18:52:21 +0300

    bla
    bla

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+"use strict";
+
+var gulp = require("gulp"),
+    del = require("del"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    watch = require("gulp-watch");
+
